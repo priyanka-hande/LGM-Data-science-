b0e2c21d928e9ceb4ff7225c254b9a887b4c3d2a
Priyanka Hande <98216228+priyanka-hande@users.noreply.github.com>
2023-01-18 18:38:14 +0530

    Update iris.py

--- iris.py
+++ iris.py
@@ -1,7 +1,7 @@
 import numpy as np 
 import pandas as pd
 import matplotlib.pyplot as plt 
-iris = pd.read_csv('/content/iriss.csv')
+iris = pd.read_csv('C:\Users\LENOVO\Documents')
 print(iris)
 iris.head()
 iris.shape
